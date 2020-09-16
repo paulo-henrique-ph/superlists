42c090e724a987c100612b3ac9b84546039f7312
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-09-16 00:08:46 -0300

    Added set of tests to do

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+# Open the Web Browser
 browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+# See if the title mentions todo list
+assert 'To-Do' in browser.title
+
+# Insert a task "Buy peacock feathers"
+
+# When inserted show the item "1: Buy peacock feathers"
+
+# Add another task "Use peacock feathers to make a fly"
+
+# Show the two items in the list
+
+# Generate a unique url for the user
+
+# Access this unique url and see if the tasks still included
+
+# End
+
+browser.quit()
