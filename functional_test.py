74e108562592826600ce9e26a47c381bd08c9d99
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-09-16 13:18:10 -0300

    Developed further functional tests

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -14,13 +16,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # See if the title mentions todo list
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test")
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
+
+
+        # See if there is an input box
+        input_box = self.browser.find_elements_by_id("id_new_item")
+        self.assertEqual(
+            input_box.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
 
         # Insert a task "Buy peacock feathers"
+        input_box.send_keys("Buy peacock feathers")
 
         # When inserted show the item "1: Buy peacock feathers"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_elements_by_id("id_list_table")
+        rows = table.find_element_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
 
         # Add another task "Use peacock feathers to make a fly"
+        self.fail("Finish the test")
 
         # Show the two items in the list
 
