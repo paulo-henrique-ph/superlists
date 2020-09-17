6b44dcbc55d95018b358effa400c26b3ce7fe8bc
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-09-17 00:06:55 -0300

    Added code to reuse in test

--- functional_test.py
+++ functional_test.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Open the Web Browser
         self.browser.get("http://localhost:8000")
@@ -34,9 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Add another task "Use peacock feathers to make a fly"
         input_box = self.browser.find_element_by_id("id_new_item")
@@ -48,10 +51,9 @@ class NewVisitorTest(unittest.TestCase):
         # Show the two items in the list
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows]
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table(
+            "2: Use peacock feathers to make a fly"
         )
 
         # Generate a unique url for the user
