06cf7c385f3233774e2fb436085a4e28a12e452f
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-09-18 10:51:00 -0300

    Added live server functional test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
 import time
-import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Open the Web Browser
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # See if the title mentions todo list
         self.assertIn("To-Do", self.browser.title)
