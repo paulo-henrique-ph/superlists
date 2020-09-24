e2a3b87c3c4b7729fc8ff5c256d52240aafd0c79
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-09-24 10:25:21 -0300

    Test for centralized input box

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -82,3 +82,15 @@ class NewVisitorTest(LiveServerTestCase):
         input_box.send_keys("Buy milk")
         input_box.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table("1: Buy milk")
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Input box is in the center
+        input_box = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            input_box.location["x"] + input_box.size["width"] / 2,
+            512,
+            delta=10
+        )
