920ce405ccd3e50954e08d84e6e1fc4b304755e9
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-10-15 23:42:07 -0300

    Separated tests files

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -1,6 +1,10 @@
 from os import environ
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
@@ -13,6 +17,26 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as err:
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as err:
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
