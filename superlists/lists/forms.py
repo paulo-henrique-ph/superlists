530c361dcb92e9bbfac5a016f5d63be9457a082c
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-02-08 10:44:56 -0300

    Implemented form validation

--- superlists/lists/forms.py
+++ superlists/lists/forms.py
@@ -1,15 +1,17 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
-EMPTY_ITEM_ERROR = "You can't habe an empty list item"
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this item in your list"
 
 
 class ItemForm(forms.models.ModelForm):
     class Meta:
         model = Item
         fields = ("text",)
-        widget = {
+        widgets = {
             "text": forms.fields.TextInput(
                 attrs={
                     "placeholder": "Enter a to-do item",
@@ -18,3 +20,22 @@ class ItemForm(forms.models.ModelForm):
             )
         }
         error_messages = {"text": {"required": EMPTY_ITEM_ERROR}}
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {"text": [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
