f8bced883890d32378cd4818c616f0679852e6f0
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2022-02-08 10:47:02 -0300

    implemented form messages on the view

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.core.exceptions import ValidationError
 
-from lists.forms import ItemForm
-from lists.models import Item, List
+from lists.forms import ExistingListItemForm, ItemForm
+from lists.models import List
 
 # Create your views here.
 def home_page(request):
@@ -11,29 +9,23 @@ def home_page(request):
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST["item_text"], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, "home.html", {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, "home.html", {"form": form})
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ExistingListItemForm(for_list=list_)
 
     if request.method == "POST":
-        try:
-            item = Item(text=request.POST["item_text"], list=list_)
-            item.full_clean()
-            item.save()
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
 
-    return render(request, "list.html", {"list": list_, "error": error})
+    return render(request, "list.html", {"list": list_, "form": form})
